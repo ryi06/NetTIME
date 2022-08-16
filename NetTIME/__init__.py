4138935bd8854abea11245c37e4e989733908747
Ren Yi <ryi06@hotmail.com>
2022-08-15 21:55:08 -0400

    Update init

--- NetTIME/__init__.py
+++ NetTIME/__init__.py
@@ -1,7 +1,3 @@
-"""
-NetTIME: A multitask learning model for prediction base-pair resolution
-in vivo TF binding predictions using embeddings.
-"""
 from .trainer import TrainWorkflow
 from .evaluator import EvaluateWorkflow
 from .predictor import PredictWorkflow
@@ -10,4 +6,4 @@ from .crf_trainer import CRFTrainWorkflow
 from .crf_evaluator import CRFEvaluateWorkflow
 from .crf_predictor import CRFPredictWorkflow
 
-__version__ = "0.1.0"
+
---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""
+NetTIME: a Multitask and Base-pair Resolution Framework for Improved 
+Transcription Factor Binding Site Prediction.
+"""
+
+__version__ = "0.1.0"
